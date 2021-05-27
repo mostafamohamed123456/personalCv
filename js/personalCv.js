bec09a50e9eb5ac84e939856bcb914b8f0a4b911
mostafa mohamed <mostafa_messi2025@yahoo.com>
2021-05-27 11:55:27 +0200

    Modify the tab icon, and js file imageChanger

--- js/personalCv.js
+++ js/personalCv.js
@@ -19,6 +19,35 @@ function chooseLink(){
         }
     }
 }
+let sliderCreativeGit = document.getElementById('creative-github');
+let sliderWeatherGit = document.getElementById('weather-github');
+let sliderBlogrGit = document.getElementById('blogr-github');
+let sliderCreativeLink = document.getElementById('creative-website');
+let sliderWeatherLink = document.getElementById('weather-website');
+let sliderBlogrLink = document.getElementById('blogr-website');
+
+sliderCreativeGit.onclick = function(){
+    window.open('https://github.com/mostafamohamed123456/Creative.git','_self','width=1600','true');
+}
+sliderWeatherGit.onclick = function(){
+    window.open('https://github.com/mostafamohamed123456/Weather.git','_self','width=1600','true');
+}
+sliderBlogrGit.onclick = function(){
+    window.open('https://github.com/mostafamohamed123456/Blogr.git','_self','width=1600','true');
+}
+sliderCreativeLink.onclick = function(){
+    window.open('https://mostafamohamed123456.github.io/Creative/','_self','width=1600','true');
+}
+sliderWeatherLink.onclick = function(){
+    window.open('https://mostafamohamed123456.github.io/Weather/','_self','width=1600','true');
+}
+sliderBlogrLink.onclick = function(){
+    window.open('https://mostafamohamed123456.github.io/Blogr/','_self','width=1600','true');
+}
+
+
+
+
 let creativeGit = document.querySelector('.portfolio-cont #creative-github');
 let weatherGit = document.querySelector('.portfolio-cont #weather-github');
 let blogrGit = document.querySelector('.portfolio-cont #blogr-github');
@@ -43,4 +72,20 @@ weatherLink.onclick = function(){
 }
 blogrLink.onclick = function(){
     window.open('https://mostafamohamed123456.github.io/Blogr/','_self','width=1600','true');
+}
+
+let backgroundImg = document.getElementById('demo');
+
+
+window.onload = () =>{
+    imageChange();
+}
+function imageChange(){
+    let imgArray = ['img/001.jpg','img/002.png','img/003.jpg'];
+    backgroundImg.style.backgroundImage = `url(${imgArray[0]})`;
+    setInterval(()=>{
+        let randomValue = Math.floor(Math.random() * imgArray.length);
+        backgroundImg.style.backgroundImage = `url(${imgArray[randomValue]})`;
+        console.log(randomValue)
+    },2000)
 }
