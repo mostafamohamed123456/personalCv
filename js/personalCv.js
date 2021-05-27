95dfc984cfe6a8e2cbdeb43f432fe8f52c51d1fd
mostafa mohamed <mostafa_messi2025@yahoo.com>
2021-05-27 11:14:36 +0200

    modify the portfolio buttons

--- js/personalCv.js
+++ js/personalCv.js
@@ -19,3 +19,28 @@ function chooseLink(){
         }
     }
 }
+let creativeGit = document.querySelector('.portfolio-cont #creative-github');
+let weatherGit = document.querySelector('.portfolio-cont #weather-github');
+let blogrGit = document.querySelector('.portfolio-cont #blogr-github');
+let creativeLink = document.querySelector('.portfolio-cont #creative-website');
+let weatherLink = document.querySelector('.portfolio-cont #weather-website');
+let blogrLink = document.querySelector('.portfolio-cont #blogr-website');
+
+creativeGit.onclick = function(){
+    window.open('https://github.com/mostafamohamed123456/Creative.git','_self','width=1600','true');
+}
+weatherGit.onclick = function(){
+    window.open('https://github.com/mostafamohamed123456/Weather.git','_self','width=1600','true');
+}
+blogrGit.onclick = function(){
+    window.open('https://github.com/mostafamohamed123456/Blogr.git','_self','width=1600','true');
+}
+creativeLink.onclick = function(){
+    window.open('https://mostafamohamed123456.github.io/Creative/','_self','width=1600','true');
+}
+weatherLink.onclick = function(){
+    window.open('https://mostafamohamed123456.github.io/Weather/','_self','width=1600','true');
+}
+blogrLink.onclick = function(){
+    window.open('https://mostafamohamed123456.github.io/Blogr/','_self','width=1600','true');
+}
